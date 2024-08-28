add444eeb70cec3535eee48d552c7fe4e6147288
Asma <saiyedas@shernet.sheridancollege.ca>
2024-08-27 22:05:09 -0400

    added bankaccount code

---
+++ src/BankAccountMain.java
@@ -0,0 +1,17 @@
+
+public class BankAccountMain {
+    public static void main(String[] args) {
+
+        BankAccount account = new BankAccount("Asma", 1000.00);
+        System.out.println ("Account Holder : " + account.getAccHold_name());
+
+        System.out.println ("Current Balance : " + account.getBalance());
+
+        account.deposit(500);
+        System.out.println ("Updated Balance : " + account.getBalance());
+
+        account.withdraw(0);
+        System.out.println ("Updated Balance : " + account.getBalance());
+    }
+
+    }
